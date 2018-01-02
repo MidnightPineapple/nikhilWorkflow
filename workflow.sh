8664d525c4904eff3325742c363de328ce78f83e
MidnightPineapple <samuel88835@yahoo.com>
2018-01-02 17:22:15 -0500

    capture stderr as well as stdout to log file

--- workflow.sh
+++ workflow.sh
@@ -34,50 +34,51 @@ cd "$P_DIR"
 echo $(date): 'STARTING PRETRIMQC'
 mkdir "$RESULTS"
 mkdir "$RESULTS"/preTrimQC
-loopThru GROUP_ALL pretrim > "$LOGS"/preTrimQC.log.out
+loopThru GROUP_ALL pretrim >> "$LOGS"/preTrimQC.log.out 2>&1
 echo $(date): 'FINISHED PRETRIMQC'
 
 echo $(date): "STARTING TRIMMOMATIC WITH COMMAND HEADCROP 13"
 mkdir "$RESULTS"/trim
-loopThru GROUP_ALL trim > "$LOGS"/trim.log.out
+loopThru GROUP_ALL trim >> "$LOGS"/trim.log.out 2>&1
 echo $(date): "FINISHED TRIMMING"
 
 echo $(date): 'STARTING POSTTRIMQC'
 mkdir "$RESULTS"/postTrimQC
-loopThru GROUP_ALL posttrim > "$LOGS"/postTrimQC.log.out
+loopThru GROUP_ALL posttrim >> "$LOGS"/postTrimQC.log.out 2>&1
 echo $(date): 'FINISHED POSTTRIMQC'
 
 if [[ ! -d $starGenome ]]; then
     echo $(date): 'MAKING STARGENOME'
     mkdir STARgenome
     starGenome="$P_DIR"/STARgenome
-    genStarGenome > "$LOGS"/genStarGenome.log.out
+    genStarGenome >> "$LOGS"/genStarGenome.log.out 2>&1
+    echo $(date): 'FINISHED MAKING STARGENOME'
 else
     echo $(date): "STARGENOME REFERENCE PROVIDED, SKIPPING STARGENOME CREATION"
 fi
 
 echo $(date): 'RUNNING STAR PASS 1'
 mkdir "$RESULTS"/STARp1
-loopThru GROUP_ALL star1 > "$LOGS"/STARp1.log.out
-echo $(date): 'FINISHED STAR P1'
+loopThru GROUP_ALL star1 >> "$LOGS"/STARp1.log.out 2>&1
+echo $(date): 'FINISHED STAR PASS 1'
 
 #makes array of all sjdb files
 formatStringArray sjdbFileString GROUP_ALL "$RESULTS/STARp1/" ".trim.SJ.out.tab"
 sjdbFiles=($sjdbFileString)
 
-echo $(date): 'RUNNING STAR P2'
+echo $(date): 'RUNNING STAR PASS 2'
 mkdir "$RESULTS"/STARp2
-loopThru GROUP_ALL star2 > "$LOGS"/STARp2.log.out
-echo $(date): 'FINISHED STAR P2'
+loopThru GROUP_ALL star2 >> "$LOGS"/STARp2.log.out 2>&1
+echo $(date): 'FINISHED STAR PASS 2'
 
 echo $(date): 'RUNNING PICARD'
 mkdir "$RESULTS"/bam_drem
-loopThru GROUP_ALL picard > "$LOGS"/picard.log.out
+loopThru GROUP_ALL picard >> "$LOGS"/picard.log.out 2>&1
 echo $(date): 'FINISHED PICARD'
 
 echo $(date): 'SUBREAD FEATURECOUNTS'
 mkdir "$RESULTS"/counts
-loopThru GROUP_ALL subread > "$LOGS"/subread.log.out
+loopThru GROUP_ALL subread >> "$LOGS"/subread.log.out 2>&1
 echo $(date): 'FINISHED FEATURECOUNTS'
 
 echo $(date): 'LIMMA VOOM ANALYSIS'
@@ -90,7 +91,7 @@ formatStringArray B_COUNTS GROUP_B "$P_DIR/$RESULTS/counts/" ".count.txt"; B_COU
 A_COUNTS_PATHS=$(joinBy , "${A_COUNTS[@]}")
 B_COUNTS_PATHS=$(joinBy , "${B_COUNTS[@]}")
 
-Rscript --vanilla "$DIR"/limmavoom.R "$P_DIR/$RESULTS/voom" "$A_COUNTS_PATHS" "$B_COUNTS_PATHS" "$GA" "$GROUP_A_NAME" "$GROUP_B_NAME" > "$LOGS"/voom.log.out
+Rscript --vanilla "$DIR"/limmavoom.R "$P_DIR/$RESULTS/voom" "$A_COUNTS_PATHS" "$B_COUNTS_PATHS" "$GA" "$GROUP_A_NAME" "$GROUP_B_NAME" >> "$LOGS"/voom.log.out 2>&1
 echo $(date): "Final results in $P_DIR/$RESULTS/voom"
 
 # removes intermediate results files if we want to reset
